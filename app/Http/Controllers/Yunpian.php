dc135ac020b2ec5022027b8b337ddadcb7c8216e
xharlie <charlie.xu007@yahoo.com>
2015-08-26 17:13:55 +0800

    ready

--- app/Http/Controllers/Yunpian.php
+++ app/Http/Controllers/Yunpian.php
@@ -18,7 +18,7 @@ class Yunpian extends Controller
 {
     public static $apikey ='26b126ce6f0379478da7c98289ee9bf7';
     public static $url="http://yunpian.com/v1/sms/send.json";
-    public static $mobiles=["18618148761"];
+    public static $mobiles=["18618148761","18092213579","18629088676"];
 
     public static function notifyFrontDeskOrder($ORDR_ID,$HTL_NM,$HTL_ID,$RM_ID,$ORDR_TSTMP,$CMB_ID,$CMB_NM,$AMNT){
         $text ="【斑鸠科技】(单号:$ORDR_ID) $HTL_NM"."酒店($HTL_ID), $RM_ID"."房间, 于$ORDR_TSTMP"."请求下单: 服务ID: $CMB_ID, 服务名称:$CMB_NM; 数量: $AMNT";
